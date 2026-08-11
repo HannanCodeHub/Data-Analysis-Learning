b630c24c0912a865aafe8b0ac38ead9297be6673
Hannanahmed <hananmeymon786@gmail.com>
2026-08-11 02:21:12 -0700

    Add NumPy broadcasting and vectorization

--- Numpy/broadcasting_ and_vectorization.py
+++ Numpy/broadcasting_ and_vectorization.py
@@ -2,6 +2,8 @@ import numpy as np
 
 #perform operation on different array (large size) -> broadcasting
 #without loops easily hojayeg and is faster .
+#expand smaller -> larger
+#1d -> 2d 
 
 prices = np.array([100,200,300,450,780,960,1000])
 
@@ -29,9 +31,24 @@ result2 = matrix + vector
 print(result2)
 
 #Incompatible shapes:
-inArray1 = np.array([[1,2,3], [4,5,6]]) 
-inArray2 = np.array([10,20]) 
+# inArray1 = np.array([[1,2,3], [4,5,6]]) 
+# inArray2 = np.array([10,20]) 
 
-res = inArray1 + inArray2
+# res = inArray1 + inArray2
 
-print(res)
+# print(res) # error !!
+# #can be solved by reshape.
+
+#VECORIZATION:
+#it is faster than loops for large data
+#used for matrix and 100x faster than loops.
+
+arr_v = np.array([10,20,30])
+arr_v2 = np.array([1,2,3])
+
+print("VECTORIZATION:")
+result_v = arr_v + arr_v2
+print(result_v)
+
+multiplied = arr_v * 3 
+print(multiplied)
